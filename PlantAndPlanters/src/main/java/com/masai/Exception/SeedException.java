c5e492470bedcc1b2908c84ae9586366e316630d
ZereFD1 <harshs381@gmail.com>
2023-05-04 23:03:39 +0530

    Worked on Exception Layer

---
+++ PlantAndPlanters/src/main/java/com/masai/Exception/SeedException.java
@@ -0,0 +1,16 @@
+package com.masai.Exception;
+
+public class SeedException extends Exception {
+
+	public SeedException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SeedException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
