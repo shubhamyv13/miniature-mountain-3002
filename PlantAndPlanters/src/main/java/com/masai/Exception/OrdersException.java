703aa080c46053a6951ac7a72077bc7969ae300c
Aman Chaudhary <aman.chaudhary214@gmail.com>
2023-05-05 22:43:48 +0530

    exceptions added day2

---
+++ PlantAndPlanters/src/main/java/com/masai/Exception/OrdersException.java
@@ -0,0 +1,15 @@
+package src.main.java.com.masai.Exception;
+
+public class OrdersException extends Exception {
+
+	public OrdersException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public OrdersException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
